394a3fc95681993d64990db79be91c4bd8bfe8ee
Jim O'Connorhorrill <oconnorhorrill@gmail.com>
2016-12-08 22:09:48 +1000

    Update Bot.java

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bot;
 
 
@@ -16,7 +12,7 @@ import java.util.Map;
 
 /**
  *
- * @author James
+ * @author James O'Connorhorrill
  */
 public class Bot {
     // Store all regular expression matches
