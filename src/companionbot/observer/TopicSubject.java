2fc5f066c8f7ee7acfed1857163163894b53cdb9
ken1kasap <ken1kasap@gmail.com>
2016-12-29 15:56:53 +0900

    add override annotation.

--- src/companionbot/observer/TopicSubject.java
+++ src/companionbot/observer/TopicSubject.java
@@ -11,14 +11,17 @@ public class TopicSubject implements Subject{
         observers = new HashSet<>();
     }
     
+    @Override
     public void addObserver(TopicObserver topicObserver) {
         observers.add(topicObserver);
     }
     
+    @Override
     public boolean deleteObserver(TopicObserver topicObserver) {
         return observers.remove(topicObserver);
     }
     
+    @Override
     public void notifyObservers() {
         observers.forEach(o -> o.update());
     }
