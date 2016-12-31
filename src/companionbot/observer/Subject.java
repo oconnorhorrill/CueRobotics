08a398f1e6e07125177c8f4d80d62a4260e3d166
ken1kasap <ken1kasap@gmail.com>
2017-01-01 01:49:39 +0900

    rename

--- src/companionbot/observer/Subject.java
+++
@@ -1,7 +0,0 @@
-package companionbot.observer;
-
-public interface Subject {
-    void addObserver(TopicObserver topicObserver);
-    boolean deleteObserver(TopicObserver topicObserver);
-    void notifyObservers();
-}
