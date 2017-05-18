9b420d62aea77ccb8f53c96c27d397d33d40edf4
Jim O'Connorhorrill <oconnorhorrill@gmail.com>
2017-05-18 20:33:39 +1000

    Create Context.java

---
+++ Old_swing/Context.java
@@ -0,0 +1,16 @@
+package bot;
+
+/**
+ *
+ * @author James
+ */
+public class Context {
+    
+    //return xml source file from node
+    private static final String source = "baseContext.xml";
+        
+    //method to get value of source
+        public static String getSource(){
+        return source;
+    }
+}
