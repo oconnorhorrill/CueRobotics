fdbc9caf3e2e33e7ec1941b6b1e222187bead52b
Jim O'Connorhorrill <oconnorhorrill@gmail.com>
2017-05-26 21:39:08 +1000

    Create VoiceObserverImpl.java

---
+++ src/companionbot/observer/VoiceObserverImpl.java
@@ -0,0 +1,21 @@
+package companionbot.observer;
+
+// This is the same as TopicObserver, So adding TopicObserver to the VoiceSubject could do this. Dupplication?
+// Might be dangerous to access Text area from two observers?
+// Think about this after doing the Router code.
+
+import javafx.scene.control.TextArea;
+
+public class VoiceObserverImpl implements TopicObserver {
+    
+    private final TextArea textArea;
+    
+    public VoiceObserverImpl(TextArea textArea) {
+        this.textArea = textArea;
+    }
+    
+    @Override
+    public void update() {
+        textArea.setText("");
+    }
+}
